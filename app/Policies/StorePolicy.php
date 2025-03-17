4b375c081af07da425acea9b3b00b31af6d70078
Jasper van der Linden <jappe999@users.noreply.github.com>
2025-03-17 07:08:39 +0100

    feat: Disallow mutations for unregistered users

---
+++ app/Policies/StorePolicy.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Store;
+use App\Models\User;
+
+class StorePolicy
+{
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(?User $user): bool
+    {
+        return $user?->isAdmin() ?? false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(?User $user, Store $store): bool
+    {
+        return $user?->isAdmin() ?? false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(?User $user, Store $store): bool
+    {
+        return $user?->isAdmin() ?? false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(?User $user, Store $store): bool
+    {
+        return $user?->isAdmin() ?? false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(?User $user, Store $store): bool
+    {
+        return $user?->isAdmin() ?? false;
+    }
+}
