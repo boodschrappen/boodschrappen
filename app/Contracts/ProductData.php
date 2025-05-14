07e70ccd5266bfa86f930fc429271a6a8b286b7a
Jasper van der Linden <jappe999@users.noreply.github.com>
2025-05-14 16:17:19 +0200

    feat: Process nutritional data

--- app/Contracts/ProductData.php
+++ app/Contracts/ProductData.php
@@ -2,15 +2,24 @@
 
 namespace App\Contracts;
 
+use App\Data\Nutrients\AllergensData;
+use App\Data\Nutrients\NutrientsData;
 use App\Models\Product;
 use App\Models\ProductStore;
 use Spatie\LaravelData\Data;
 
 interface ProductData
 {
+    public static function fromModel(ProductStore $storeProduct): Data;
+
     public function toProduct(): Product;
 
     public function toStoreProduct(): ProductStore;
 
-    public static function fromModel(ProductStore $storeProduct): Data;
+    public function nutrients(): NutrientsData;
+
+    /** @return Array<string> */
+    public function ingredients(): array;
+
+    public function allergens(): AllergensData;
 }
--- app/Data/DirkProductData.php
+++ app/Data/DirkProductData.php
@@ -3,75 +3,123 @@
 namespace App\Data;
 
 use App\Contracts\ProductData;
+use App\Data\Nutrients\AllergensData;
+use App\Data\Nutrients\NutrientsData;
 use App\Models\Product;
 use App\Models\ProductStore;
+use Illuminate\Support\Arr;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Optional;
 
 class DirkProductData extends Data implements ProductData
 {
     public function __construct(
-        public int                  $ProductID,
-        public string               $MainDescription,
-        public array                $ProductPrices,
+        public int $ProductID,
+        public string $MainDescription,
+        public array $ProductPrices,
         public Optional|null|string $ProductNumber,
         public Optional|null|string $SubDescription = null,
         public Optional|null|string $CommercialContent = null,
         public Optional|null|string $MaxPerCustomer = null,
         public Optional|null|string $DepositMoney = null,
         public Optional|null|string $Brand = null,
-        public Optional|null|bool   $ProductOnline = null,
-        public Optional|null|bool   $ProductInStock = null,
-        public Optional|null|bool   $WeightArticle = null,
-        public Optional|null|bool   $ScaleIndicator = null,
+        public Optional|null|bool $ProductOnline = null,
+        public Optional|null|bool $ProductInStock = null,
+        public Optional|null|bool $WeightArticle = null,
+        public Optional|null|bool $ScaleIndicator = null,
         public Optional|null|string $PiecesInWeight = null,
         public Optional|null|string $AlcoholPercentage = null,
-        public Optional|null|bool   $TemporaryNotAvailable = null,
+        public Optional|null|bool $TemporaryNotAvailable = null,
         public Optional|null|string $PublicationAfter = null,
         public Optional|null|string $WeightOfPeicesInWeight = null,
-        public Optional|null|array  $ProductPicture = null,
-        public Optional|null|array  $ProductPictures = null,
-        public Optional|null|array  $Logos = null,
-        public Optional|null|array  $WebSubGroups = null,
-        public Optional|null|array  $ProductOffers = null,
-        public Optional|null|array  $StoreAssortments = null,
-        public Optional|null|bool   $IsSingleUsePlastic = null,
+        public Optional|null|array $ProductPicture = null,
+        public Optional|null|array $ProductPictures = null,
+        public Optional|null|array $Logos = null,
+        public Optional|null|array $WebSubGroups = null,
+        public Optional|null|array $ProductOffers = null,
+        public Optional|null|array $StoreAssortments = null,
+        public Optional|null|bool $IsSingleUsePlastic = null,
 
         // Only available on details page.
-        public Optional|null|array  $Nutrition = null,
-        public Optional|null|array  $ProductDeclarations = null,
-        public Optional|null|array  $ProductBarcodes = null,
+        public Optional|null|array $Nutrition = null,
+        public Optional|null|array $ProductDeclarations = null,
+        public Optional|null|array $ProductBarcodes = null
     ) {}
+
+    public static function fromModel(ProductStore $storeProduct): self
+    {
+        return self::from([
+            "id" => $storeProduct->raw_identifier,
+            "MainDescription" => $storeProduct->product->name,
+            "ProductPrices" => [
+                "RegularPrice" => $storeProduct->original_price,
+            ],
+        ]);
+    }
 
     public function toProduct(): Product
     {
         return new Product([
-            'gtins'       => $this->ProductBarcodes ? [$this->ProductBarcodes[0]['Barcode']] : [],
-            'name'        => $this->Brand . ' ' . $this->MainDescription,
-            'summary'     => $this->SubDescription ?? '',
-            'description' => '',
-            'image'       => $this->ProductPictures ? $this->ProductPictures[0]['Url'] : $this->ProductPicture['Url'] ?? null,
+            "gtins" => $this->ProductBarcodes
+                ? [$this->ProductBarcodes[0]["Barcode"]]
+                : [],
+            "name" => $this->Brand . " " . $this->MainDescription,
+            "summary" => $this->SubDescription ?? "",
+            "description" => "",
+            "image" => $this->ProductPictures
+                ? $this->ProductPictures[0]["Url"]
+                : $this->ProductPicture["Url"] ?? null,
         ]);
     }
 
     public function toStoreProduct(): ProductStore
     {
         return new ProductStore([
-            'raw_identifier' => $this->ProductID,
-            'reduced_price' => $this->ProductPrices[0]['Price'],
-            'original_price' => $this->ProductPrices[0]['RegularPrice'],
-            'raw' => $this->toArray(),
+            "raw_identifier" => $this->ProductID,
+            "reduced_price" => $this->ProductPrices[0]["Price"],
+            "original_price" => $this->ProductPrices[0]["RegularPrice"],
+            "raw" => $this->toArray(),
         ]);
     }
 
-    public static function fromModel(ProductStore $storeProduct): self
+    public function nutrients(): NutrientsData
     {
-        return self::from([
-            'id' => $storeProduct->raw_identifier,
-            'MainDescription' => $storeProduct->product->name,
-            'ProductPrices' => [
-                'RegularPrice' => $storeProduct->original_price,
+        $rowFn = fn($row) => [$row["Text"], $row["ValueAsSold"]];
+
+        return NutrientsData::from([
+            "headings" => [
+                "",
+                $this->ProductDeclarations[0]["NutritionInformation"][
+                    "Voedingswaarden"
+                ]["Algemeen"]["Standaardeenheid"],
             ],
+            "rows" => Arr::flatten(
+                array_map(
+                    fn($row) => [
+                        $rowFn($row["MainValue"]),
+                        ...array_map($rowFn, $row["SubValues"]),
+                    ],
+                    $this->Nutrition
+                )
+            ),
         ]);
+    }
+
+    public function ingredients(): array
+    {
+        return [$this->ProductDeclarations[0]["ProductIngredients"][0]["Text"]];
+    }
+
+    public function allergens(): AllergensData
+    {
+        return new AllergensData(
+            array_map(
+                fn($allergen) => implode(" ", [
+                    $allergen["AllergenText"],
+                    $allergen["AllergenDescription"],
+                ]),
+                $this->ProductDeclarations[0]["ProductAllergens"]
+            )
+        );
     }
 }
