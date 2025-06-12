0d6a65f967fd280551a7819fba505d78d2546f3c
Jasper van der Linden <jappe999@users.noreply.github.com>
2025-06-12 22:57:19 +0200

    fix: Specify what happens to shopping list items

--- app/Policies/ShoppingListItemPolicy.php
+++ app/Policies/ShoppingListItemPolicy.php
@@ -16,51 +16,55 @@ class ShoppingListItemPolicy
         return $user !== null;
     }
 
-    // /**
-    //  * Determine whether the user can view the model.
-    //  */
-    // public function view(User $user, ShoppingListItem $shoppingListItem): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, ShoppingListItem $shoppingListItem): bool
+    {
+        return $user?->id === $shoppingListItem->user_id;
+    }
 
-    // /**
-    //  * Determine whether the user can create models.
-    //  */
-    // public function create(User $user): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user !== null;
+    }
 
-    // /**
-    //  * Determine whether the user can update the model.
-    //  */
-    // public function update(User $user, ShoppingListItem $shoppingListItem): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, ShoppingListItem $shoppingListItem): bool
+    {
+        return $user?->id === $shoppingListItem->user_id;
+    }
 
-    // /**
-    //  * Determine whether the user can delete the model.
-    //  */
-    // public function delete(User $user, ShoppingListItem $shoppingListItem): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, ShoppingListItem $shoppingListItem): bool
+    {
+        return $user?->id === $shoppingListItem->user_id;
+    }
 
-    // /**
-    //  * Determine whether the user can restore the model.
-    //  */
-    // public function restore(User $user, ShoppingListItem $shoppingListItem): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(
+        User $user,
+        ShoppingListItem $shoppingListItem
+    ): bool {
+        return $user?->id === $shoppingListItem->user_id;
+    }
 
-    // /**
-    //  * Determine whether the user can permanently delete the model.
-    //  */
-    // public function forceDelete(User $user, ShoppingListItem $shoppingListItem): bool
-    // {
-    //     return false;
-    // }
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(
+        User $user,
+        ShoppingListItem $shoppingListItem
+    ): bool {
+        return $user?->id === $shoppingListItem->user_id;
+    }
 }
